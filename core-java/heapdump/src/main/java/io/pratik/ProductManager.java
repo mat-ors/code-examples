0d72b1561bab5a75642a88167ff05266383ff51d
Matisz Orsolya <orsolya.matisz@frontendart.com>
2021-08-11 14:41:16 +0200

    Testing complexity

--- core-java/heapdump/src/main/java/io/pratik/ProductManager.java
+++ core-java/heapdump/src/main/java/io/pratik/ProductManager.java
@@ -21,11 +21,63 @@ public class ProductManager {
 	public void populateProducts() {
 
 		int dummyArraySize = 1;
+		while (dummyArraySize == 1) {
+			System.out.println("dummyArraySize is 1");
+		}
 		for (int loop = 0; loop < 10; loop++) {
 			if(loop%2 == 0) {
-			  createObjects(regularItems, dummyArraySize);
-			}else {
+				for(int i = 1500; i >= 15000; i++) {
+					loop++;
+					System.out.println("sadasda");
+				}
+				createObjects(regularItems, dummyArraySize);
+			} else {
 			  createObjects(discountedItems, dummyArraySize);
+			}
+			int num;
+			int num2;
+			if(num2 == 0) {
+				switch (num) {
+					case 0:
+						return 1;
+					case 1:
+						return 2;
+					case 2:
+						return 3;
+					case 3:
+						return 1;
+					case 4:
+						return 2;
+					case 5:
+						return 3;
+					case 6:
+						return 1;
+					case 7:
+						return 2;
+					case 8:
+						return 3;
+					case 9:
+						return 1;
+					case 10:
+						return 2;
+					case 11:
+						return 3;
+					case 12:
+						return 1;
+					case 13:
+						return 2;
+					case 14:
+						return 3;
+					default:
+						return 0;
+				} else {
+					for (int num = 0; num < 10000; num++) {
+						num++;
+						if (num%2==0){
+							System.out.println("num is even")
+						}
+					}
+				}
 			}
 			System.out.println("Memory Consumed till now: " + loop + "::"+ regularItems + " "+discountedItems );
 			dummyArraySize *= dummyArraySize * 2;
