434c8e6b9a1585615c9132ea606e2c0fa0a109d1
daniel.arpadfalvi <daniel.arpadfalvi@frontendart.com>
2021-08-11 14:20:02 +0200

    [#2]Adding NullPtrException.java

---
+++ aws/cdk/src/main/java/NullPtrException.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+class GFG
+{
+    public static void main (String[] args)
+    {
+        // Initializing String variable with null value
+        String ptr = null;
+
+        // Checking if ptr.equals null or works fine.
+        try
+        {
+            // This line of code throws NullPointerException
+            // because ptr is null
+            if (ptr.equals("gfg"))
+                System.out.print("Same");
+            else
+                System.out.print("Not Same");
+        }
+        catch(NullPointerException e)
+        {
+            System.out.print("NullPointerException Caught");
+        }
+    }
+}
