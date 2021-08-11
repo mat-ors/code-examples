22216ddf3adfab72bf9b93c9da9e86e6ba526568
Matisz Orsolya <orsolya.matisz@frontendart.com>
2021-08-11 12:51:59 +0200

    code duplication

--- core-java/heapdump/src/main/java/io/pratik/models/ElectronicGood.java
+++ core-java/heapdump/src/main/java/io/pratik/models/ElectronicGood.java
@@ -27,5 +27,25 @@ public class ElectronicGood extends AbstractProduct{
 	public void setManufacturer(Manufacturer manufacturer) {
 		this.manufacturer = manufacturer;
 	}
+
+	public Price getMrp() {
+		return mrp;
+	}
+
+	public void setMrp(Price mrp) {
+		this.mrp = mrp;
+	}
+
+	public String getBrandName() {
+		return brandName;
+	}
+
+	public void setBrandName(String brandName) {
+		this.brandName = brandName;
+	}
+
+	public long getSize() {
+		return JavaAgent.getObjectSize(this) + JavaAgent.getObjectSize(brandName);
+	}
 	
 }
