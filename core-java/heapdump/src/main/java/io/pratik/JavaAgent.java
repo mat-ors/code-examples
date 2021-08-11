cd7ba4b9d539a600b368c242fa8e2d372d644c93
Matisz Orsolya <orsolya.matisz@frontendart.com>
2021-08-11 13:55:57 +0200

    Another code duplication

--- core-java/heapdump/src/main/java/io/pratik/JavaAgent.java
+++ core-java/heapdump/src/main/java/io/pratik/JavaAgent.java
@@ -20,4 +20,19 @@ public class JavaAgent {
         }
         return instrumentation.getObjectSize(object);
     }
+
+    public ClassicsLibrary() {
+        books = new TreeMap<>();
+        Book nineteenEightyFour = new Book("Nineteen Eighty-Four", "George Orwell",
+                "It was a bright cold day in April, and the clocks were striking thirteen.");
+        Book theLordOfTheRings = new Book("The Lord of the Rings", "J. R. R. Tolkien",
+                "When Mr. Bilbo Baggins of Bag End announced that he would shortly be celebrating his " +
+                        "eleventy-first birthday with a party of special magnificence, there was much talk and excitement in Hobbiton.");
+        Book cleanCode = new Book("Clean Code", "Robert C. Martin",
+                "ClassicsEven bad code can function. But if code isn’t clean, it can bring a development organization to its knees");
+
+        books.put("Nineteen Eighty-Four", nineteenEightyFour);
+        books.put("The Lord of the Rings", theLordOfTheRings);
+        books.put("Clean Code", cleanCode);
+    }
 }
